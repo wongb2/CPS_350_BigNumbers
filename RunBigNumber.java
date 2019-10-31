3783e3519367a21e4cf938098649099a4d85816f
dport17 <57191783+dport17@users.noreply.github.com>
2019-10-30 23:06:21 -0400

    Just noted stuff to ask about later

--- RunBigNumber.java
+++ RunBigNumber.java
@@ -7,7 +7,8 @@ public class RunBigNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		//just putting this note here because I think you know something I don't about why we'd use the new String() constructor
+		//here instead of making it a string literal and I don't want to forget to ask.
 		String s = new String("250");
 		BigNumber number = new BigNumber(s);
 		//BigNumber copy = new BigNumber(number);
@@ -15,6 +16,7 @@ public class RunBigNumber {
 		BigNumber diff = new BigNumber("260");
 		
 		BigNumber outcome = new BigNumber("11");
+		//More questions here I want to ask about.
 		outcome = BigNumber.sub(number, diff);
 		
 		outcome.display_bigEnd();
